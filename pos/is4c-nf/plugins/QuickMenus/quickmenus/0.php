bac0b0e4705af9991221d2507f95c036c7743201
Joel Brock <joelbrock@gmail.com>
2012-12-22 13:21:09 -0500

    QUICK MENUS!

--- pos/is4c-nf/plugins/QuickMenus/quickmenus/0.php
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/0.php
@@ -1,24 +1,20 @@
 <?php
 
 $my_menu = array(
-	"Grocery" => "DP10",
-	"Perishable Grocery" => "DP20",
-	"Dairy" => "DP30",
-	"Produce" => "DP40",
-	"Frozen" => "DP50",
-	"Bulk" => "DP60",
-	"Bulk Spice & Tea" => "DP70",
-	"Bread" => "DP80",
-	"Deli & Prepared Food" => "DP90",
-	"Beer" => "DP100",
-	"Wine" => "DP110",
-	"Cheese" => "DP120",
-	"Meat & Seafood" => "DP130",
-	"Vitamins & Supplements" => "DP140",
-	"Body Care & Beauty" => "DP150",
-	"General Merchandise" => "DP160",
-	"Pre-Order" => "DP170",
-	"Taxable Pre-Order" => "DP180"
+	"Cash" => "CA",
+	"Check" => "CK",
+	"Credit Card" => "CC",
+	"Debit Card" => "DC",
+	"SNAP - Food" => "FS",
+	"SNAP - Cash" => "EC",
+	"Gift Certificate" => "TC",
+	"WIC" => "WI",
+	"Store Charge" => "MI",
+	"Vendor Coupon" => "MC",
+	"In-Store Coupon" => "IC",
+	"Travelers Check" => "TV",
+	"MAD Coupon" => "MA",
+	"ELFCo Bucks" => "EL"
 );
 
 ?>
---
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/2.php
@@ -0,0 +1,25 @@
+<?php
+
+$my_menu = array(
+	"Grocery ND" => "DP410",
+	"Perishable Grocery ND" => "DP420",
+	"Dairy ND" => "DP430",
+	"Produce ND" => "DP440",
+	"Frozen ND" => "DP450",
+	"Bulk ND" => "DP460",
+	"Bulk Spice & Tea ND" => "DP470",
+	"Bread ND" => "DP480",
+	"Deli & Prepared Food ND" => "DP490",
+	"Beer ND" => "DP4100",
+	"Wine ND" => "DP4110",
+	"Cheese ND" => "DP4120",
+	"Meat & Seafood ND" => "DP4130",
+	"Vitamins & Supplements ND" => "DP4140",
+	"Body Care & Beauty ND" => "DP4150",
+	"General Merchandise ND" => "DP4160",
+	"Garden Supply ND" => "DP4190",
+	"Pre-Order ND" => "DP4170",
+	"Taxable Pre-Order ND" => "DP4180"
+);
+
+?>
