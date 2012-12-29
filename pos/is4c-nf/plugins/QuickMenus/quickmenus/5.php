fd4c29890992485b42ad0c5f9af7ed6181c35015
Joel Brock <joelbrock@gmail.com>
2012-12-28 23:27:23 -0500

    escape $s

--- pos/is4c-nf/plugins/QuickMenus/quickmenus/5.php
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/5.php
@@ -1,15 +1,16 @@
 <?php
 
 $my_menu = array(
-	"General ELFCo Donation - $X.XX" => "DP850",
-	"Haven House Donation - $1" => "1025",
-	"Haven House Donation - $7" => "1027",
-	"Haven House Donation - $20" => "1028",
-	"Christmas Tree - $25" => "1225",
-	"Peace Donation - $1" => "9901",
-	"Buttermilk Jamboree - $10" => "20015",
-	"Buttermilk Jamboree - $60" => "8406200154",
-	"Dental Donation - $1" => "7732601083"
+	"General ELFCo Donation - \$X.XX" => "DP850",
+	"" => "",
+	"Haven House Donation - \$1" => "1025",
+	"Haven House Donation - \$7" => "1027",
+	"Haven House Donation - \$20" => "1028",
+	"Christmas Tree - \$25" => "1225",
+	"Peace Donation - \$1" => "9901",
+	"Buttermilk Jamboree - \$10" => "20015",
+	"Buttermilk Jamboree - \$60" => "8406200154",
+	"Dental Donation - \$1" => "7732601083"
 );
 
 ?>
