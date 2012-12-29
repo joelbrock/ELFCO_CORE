4febbdac94ab54eb28fee8214d3435a0e941ac84
Joel Brock <joelbrock@gmail.com>
2012-12-28 23:22:23 -0500

    more custom quickmenus

---
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/4.php
@@ -0,0 +1,10 @@
+<?php
+
+$my_menu = array(
+	"OPEN BOTTLE DEPOSIT - $X.XX" => "DP810",
+	"Absopure Deposit - $7.00" => "2604",
+	"Serra Basket Dep. - $1.00" => "99913",
+	"Calder Bottle Dep. - $1.50" => "99911"
+);
+
+?>
---
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/5.php
@@ -0,0 +1,15 @@
+<?php
+
+$my_menu = array(
+	"General ELFCo Donation - $X.XX" => "DP850",
+	"Haven House Donation - $1" => "1025",
+	"Haven House Donation - $7" => "1027",
+	"Haven House Donation - $20" => "1028",
+	"Christmas Tree - $25" => "1225",
+	"Peace Donation - $1" => "9901",
+	"Buttermilk Jamboree - $10" => "20015",
+	"Buttermilk Jamboree - $60" => "8406200154",
+	"Dental Donation - $1" => "7732601083"
+);
+
+?>
