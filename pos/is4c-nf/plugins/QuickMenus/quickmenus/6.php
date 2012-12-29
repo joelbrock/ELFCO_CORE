3dbd663a8eeaebf9e51ec511e083b49f2281c8a8
Joel Brock <joelbrock@gmail.com>
2012-12-29 09:07:36 -0500

    changes

---
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/6.php
@@ -0,0 +1,11 @@
+<?php
+
+$my_menu = array(
+	"OPEN BOTTLE RETURN - \$X.XX" => "5",
+	"" => "",
+	"10¢ Bottle Return" => "2605",
+	"Absopure Bottle Return - \$7.00" => "2603",
+	"Serra Basket Return - \$1.00" => "99914"
+);
+
+?>
