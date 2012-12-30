1c46b27e9d919c5e08d14d2f474bf26a014f44ee
Joel Brock <joelbrock@gmail.com>
2012-12-30 13:21:28 -0500

    ,

--- pos/is4c-nf/plugins/QuickMenus/quickmenus/3.php
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/3.php
@@ -1,7 +1,7 @@
 <?php
 
 $my_menu = array(
-	"FULL SHARE PMT - \$60" => "499099"
+	"FULL SHARE PMT - \$60" => "499099",
 	"LOW-INCOME JOIN FEE" => "2892",
 	"STUDENT JOIN FEE" => "2893",
 	"QUARTERLY SINGLE" => "2895",
