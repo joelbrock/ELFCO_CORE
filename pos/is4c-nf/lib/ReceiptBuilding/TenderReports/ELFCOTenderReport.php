d634bda80c73a0957031f8074cacee669923fed5
Joel Brock <joelbrock@gmail.com>
2014-10-24 11:40:42 -0700

    cash reporting on TR undo updates

--- pos/is4c-nf/lib/ReceiptBuilding/TenderReports/ELFCOTenderReport.php
+++ pos/is4c-nf/lib/ReceiptBuilding/TenderReports/ELFCOTenderReport.php
@@ -68,14 +68,7 @@ static public function get(){
 		$query = "select tdate,register_no,trans_no,tender
 		       	from TenderTapeGeneric where emp_no=".$CORE_LOCAL->get("CashierNo").
 			" and trans_subtype = '".$tender_code."' order by tdate";
-		switch($tender_code){
-		case 'CA':
-			$query = "select tdate,register_no,trans_no,-total AS tender
-				from dlog where emp_no=".$CORE_LOCAL->get("CashierNo").
-				" and trans_type='T' AND trans_subtype IN ('CA','DCCB','CKCB')
-				  ORDER BY tdate";
-			break;
-		}
+
 		$result = $db_a->query($query);
 		$num_rows = $db_a->num_rows($result);
 
