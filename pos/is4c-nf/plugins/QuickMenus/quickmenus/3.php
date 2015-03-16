2a8c09d71d360d7bccda73db09c43563fff75dbe
joel brock <joelbrock@gmail.com>
2015-03-16 14:40:52 -0400

    clean up equity QM options

--- pos/is4c-nf/plugins/QuickMenus/quickmenus/3.php
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/3.php
@@ -2,13 +2,7 @@
 
 $my_menu = array(
 	"Additional Equity Investment" => "DP830",
-	"New Equity Investment" => "DP840",
-	"" => "",
-	"FULL SHARE PMT - \$60" => "499099",
-	"LOW-INCOME JOIN FEE - \$5" => "2892",
-	"STUDENT JOIN FEE - \$12" => "2893",
-	"QUARTERLY SINGLE - \$7.50" => "2895",
-	"QUARTERLY FAMILY - \$15" => "2896"
+	"New Equity Investment" => "DP840"
 );
 
 ?>
